4274b3dd17b0235d4edf54540b9591a56840343f
realMorgon <moritz.olthoff@t-online.de>
2026-09-24 13:33:18 +0200

    update migration to include equipped state

--- migrations/src/main/resources/db/migration/V0.1__Add_wardrobe_table.sql
+++ migrations/src/main/resources/db/migration/V0.1__Add_wardrobe_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS wardrobe (
     player_uuid uuid NOT NULL,
-    cosmetic_key varchar(255) NOT NULL
+    cosmetic_key varchar(255) NOT NULL,
+    is_equipped bool NOT NULL
 )
